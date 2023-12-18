6abdafd5492f123666dbb655e70ff5a8f5389695
dil-chhetri <137667394+dil-chhetri@users.noreply.github.com>
2023-12-18 13:52:28 +0545

    job and company module

--- react/src/views/Dashboard.jsx
+++ react/src/views/Dashboard.jsx
@@ -1,4 +1,44 @@
+import { useEffect, useState } from "react"
+import axiosClient from "../axios-client";
+import { Link } from "react-router-dom";
+import { useStateContext } from "../contexts/ContextProvider";
+
+
 export default function Dashboard(){
+    const [jobs, setJobs] = useState([]);
+    const [companies, setCompanies] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(()=>{
+        getCompanies();
+        getJobs();
+    }, [])
+
+    const getCompanies = () =>{
+        setLoading(true)
+        axiosClient.get('/companies')
+        .then(({data})=>{
+            setLoading(false)
+            setCompanies(data.data)
+            console.log(data)
+        })
+        .catch(
+            setLoading(false)
+        )
+    }
+
+    const getJobs = () =>{
+        setLoading(true)
+        axiosClient.get('/jobs')
+        .then(({data})=>{
+            setLoading(false)
+            setJobs(data.data)
+            console.log(data)
+        })
+        .catch(
+            setLoading(false)
+        )
+    }
     return (
         <div>
             <div className="row justify-content-center">
@@ -8,56 +48,50 @@ export default function Dashboard(){
                 <div className="col-md-4 card mx-5 shadow"><h4>Categories</h4></div>
                 <div className="col-md-10 card shadow mt-5 justify-content-center ">
                     <h4>Jobs</h4>
-                    <div className=" d-flex flex-wrap justify-content-center">
-                    <div className="w-25 card shadow mx-2">job1</div>
-                    <div className="w-25 card shadow mx-2">job2</div>
-                    <div className="w-25 card shadow mx-2">job3</div>
-                    <div className="w-25 card shadow mx-2">job4</div>
-                    <div className="w-25 card shadow mx-2">job5</div>
-                    <div className="w-25 card shadow mx-2">job6</div>
-                    <div className="w-25 card shadow mx-2">job7</div>
-                    <div className="w-25 card shadow mx-2">job8</div>
-                    <div className="w-25 card shadow mx-2">job9</div>
-
-
-
-                    </div>
+                    {loading && 
+                            <div className=" d-flex flex-wrap justify-content-center"></div>
+                        }
+                        {!loading && 
+                        <div className=" d-flex flex-wrap justify-content-center">
+                        {jobs.map(u =>(
+                            <div key={u.id} className="w-25 card shadow mx-2">{u.job_title}</div>
+                        ))}
+                         </div>
+                        }
                 </div>
             <div className="row  justify-content-around">
                 <div className="col-md-5 card shadow mt-5 justify-content-center ">
                     <h4>Top Companies</h4>
-                    <div className=" d-flex flex-wrap justify-content-center">
-                    <div className="w-25 card shadow mx-2">clogo1</div>
-                    <div className="w-25 card shadow mx-2">clogo2</div>
-                    <div className="w-25 card shadow mx-2">clogo3</div>
-                    <div className="w-25 card shadow mx-2">clog4</div>
-                    <div className="w-25 card shadow mx-2">clog5</div>
-                    <div className="w-25 card shadow mx-2">clogo6</div>
-                    <div className="w-25 card shadow mx-2">clogo7</div>
-                    <div className="w-25 card shadow mx-2">clogo8</div>
-                    <div className="w-25 card shadow mx-2">clogo9</div>
+                    
+                        {loading && 
+                            <div className=" d-flex flex-wrap justify-content-center"></div>
+                        }
+                        {!loading && 
+                        <div className=" d-flex flex-wrap justify-content-center">
+                        {companies.map(u =>(
+                            <div key={u.id} className="w-25 card shadow mx-2">{u.company_name}</div>
+                        ))}
+                         </div>
+                        }
+
 
 
 
                     </div>
-                </div>
+               
 
                 <div className="col-md-5 card shadow mt-5 justify-content-center ">
                     <h4>Top Companies</h4>
+                    {loading && 
+                        <div className=" d-flex flex-wrap justify-content-center"></div>
+                    }
+                    {!loading && 
                     <div className=" d-flex flex-wrap justify-content-center">
-                    <div className="w-25 card shadow mx-2">clogo1</div>
-                    <div className="w-25 card shadow mx-2">clogo2</div>
-                    <div className="w-25 card shadow mx-2">clogo3</div>
-                    <div className="w-25 card shadow mx-2">clog4</div>
-                    <div className="w-25 card shadow mx-2">clog5</div>
-                    <div className="w-25 card shadow mx-2">clogo6</div>
-                    <div className="w-25 card shadow mx-2">clogo7</div>
-                    <div className="w-25 card shadow mx-2">clogo8</div>
-                    <div className="w-25 card shadow mx-2">clogo9</div>
-
-
-
+                    {companies.map(u =>(
+                        <div key={u.id} className="w-25 card shadow mx-2">{u.company_name}</div>
+                    ))}
                     </div>
+                    }
                 </div>
 
                 </div>
